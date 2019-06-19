0c8df0695907f6ce62e43127179dc9706c529401
Augustin Rieunier <arieunier@arieunier-ltm2.internal.salesforce.com>
2019-06-19 16:46:15 +0200

    update on deployment script

--- deploy.sh
+++ deploy.sh
@@ -14,27 +14,38 @@ APPLICATION_NAME=$1
 echo "######### Creating the app"
 heroku apps:create $APPLICATION_NAME --region eu 
 
+echo "######### Adding Coralogix"
+heroku addons:create coralogix:dev --app $APPLICATION_NAME
+
 echo "######### Adding Heroku Postgres addon"git
 heroku addons:create heroku-postgresql --app $APPLICATION_NAME
 
+echo "######### Creates databases " 
+heroku pg:psql -f createTables.sql --app $APPLICATION_NAME
+
 echo "######### Adding Heroku Connect addon"
+HEROKU_API_KEY=`heroku auth:token`
+export HEROKU_API_KEY
 heroku addons:create herokuconnect --app $APPLICATION_NAME
+
+echo "######### Configuring Heroku Connect Addon"
+#echo "Enter org name:"
+#read -s orgname
+#echo "Enter Org Password:"
+#read -s password
+#sfdx shane:heroku:connect -a $APPLICATION_NAME -e production -f jraulouvre.json -u $orgname -p $password > /dev/null
 
 echo "######### Adding Redis addon"
 heroku addons:create heroku-redis:premium-0 --app $APPLICATION_NAME
-
-echo "######### Adding LogDNA"
-heroku addons:create logdna:atto --app $APPLICATION_NAME
-
 
 echo "######### Adding Loaderio"
 heroku addons:create loaderio:basic --app $APPLICATION_NAME
 
 echo "######### Adding sendgrid"
-heroku addons:create sendgrid:starter --app $APPLICATION_NAME
-#heroku config:set SENDGRID_API_KEY=`heroku config:get SENDGRID_API_KEY --app jraulouvre` --app $APPLICATION_NAME
-#heroku config:set SENDGRID_PASSWORD=`heroku config:get SENDGRID_PASSWORD --app jraulouvre` --app $APPLICATION_NAME
-#heroku config:set SENDGRID_USERNAME=`heroku config:get SENDGRID_USERNAME --app jraulouvre` --app $APPLICATION_NAME
+#heroku addons:create sendgrid:starter --app $APPLICATION_NAME
+heroku config:set SENDGRID_API_KEY=`heroku config:get SENDGRID_API_KEY --app jraulouvre` --app $APPLICATION_NAME
+heroku config:set SENDGRID_PASSWORD=`heroku config:get SENDGRID_PASSWORD --app jraulouvre` --app $APPLICATION_NAME
+heroku config:set SENDGRID_USERNAME=`heroku config:get SENDGRID_USERNAME --app jraulouvre` --app $APPLICATION_NAME
 
 echo "######### Adding New relic"
 heroku addons:create newrelic:hawke --app $APPLICATION_NAME
@@ -43,10 +54,6 @@ newrelic-admin generate-config $NEW_RELIC_LICENSE_KEY newrelic.ini
 heroku config:add NEW_RELIC_CONFIG_FILE='/app/newrelic.ini'  --app $APPLICATION_NAME
 heroku config:add NEW_RELIC_LOG_LEVEL=debug --app $APPLICATION_NAME
 heroku config:set NEW_RELIC_APP_NAME="$APPLICATION_NAME" --app $APPLICATION_NAME
-
-
-echo "######### Creates databases " 
-heroku pg:psql -f createTables.sql --app $APPLICATION_NAME
 
 echo "######### adding other environment variables"
 
